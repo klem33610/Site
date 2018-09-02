090dd97a755d4eddf98b9649f5e8b01f160a2292
klem <klem33@gmail.com>
2018-09-03 00:59:00 +0200

    blabla 	modifié :         Site internet/Formulaire.html 	modifié :         Site internet/static/php/post_BDD.php 	Site internet/Formulaire.php

---
+++ Site internet/static/php/post_BDD.php
@@ -0,0 +1,32 @@
+<?php
+// Connexion à la base de données
+$_POST['submit'];
+try
+
+{
+
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+
+}
+
+catch(Exception $e)
+
+{
+
+        die('Erreur : '.$e->getMessage());
+
+}
+
+
+// Insertion du message à l'aide d'une requête préparée
+
+$req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
+
+$req->execute(array($_POST['pseudo'], $_POST['message']));
+
+
+// Redirection du visiteur vers la page du minichat
+
+header('Location: minichat.php');
+
+?>
