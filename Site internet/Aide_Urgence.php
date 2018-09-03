d174deddd6e798eba4fd2ba93c41d76cf5c8ada0
Clément DESAGE <clement.desage@rte-france.com>
2018-09-03 15:50:25 +0200

    Découpage fichiers HTML en PHP (+ include)

---
+++ Site internet/Aide_Urgence.php
@@ -0,0 +1,45 @@
+<?php include('Base.php'); ?>
+<body style="color:grey">
+  <div class="text-center mt-2" style="font-family: Arial; color: rgb(204, 153, 51)">
+    <h4 class="font-weight-bold">PROGRAMME <span>
+      <a href="../jarez-solidarites.html"><img style="height: 100px;" alt="Logo Jarez Solidarités" src="static/media/logoJS.jpg"></img></a>
+    </span>AIDE D'URGENCE</h4>
+  </div>
+  <div class="row col-sm-12 mt-2">
+    <div class="col-sm-3">
+      <h4 class="text-center">Le projet humanitaire</h4>
+      <ul>
+        <li class="font-weight-bold">Pour qui ?</li>
+        <ul>
+          <li>Personnes en difficulté, notamment à la rue</li>
+          <li>Trois familles hébergées temporairement par des bénévoles</li>
+        </ul>
+        <li class="font-weight-bold">Pour quoi ?</li>
+        <ul>
+          <li>Mises à disposition de logements, fourniture de nourriture et produits de 1ère nécessité</li>
+        </ul>
+        <li class="font-weight-bold">Comment ?</li>
+        <ul>
+          <li>Financement par dons solidaires mensuels réguliers</li>
+        </ul>
+      </ul>
+    </div>
+    <?php include('Formulaire.php'); ?>
+    <div class="col-sm-3">
+      <h4 class="text-center">Avancement au 25 août</h4>
+      <ul>
+        <li>Un grand <span class="font-weight-bold">MERCI</span> aux 37 personnes qui ont effectué leurs 1ers versements mensuels. Ils s'élevent à :</li>
+        <ul>
+          <li>565 € / mois pour le mois de juillet</li>
+          <li>445 € / mois pour le mois d'août</li>
+        </ul>
+        <li>Reste à recevoir les versements de 9 personnes qui avaient exprimé leur intention</li>
+        <li>Et bien sûr, <span class="font-weight-bold">tout le monde</span> peut participer en remplissant le formulaire ci-dessous !</li>
+        <ul>
+          <li>Tout excédent après paiement du loyer et charges sera affecté à ce projet !</li>
+        </ul>
+        <li>Les formalités de mise à disposition de 3 appartements à St-Etienne s'achèvent et les 3 familles doivent s'installer le 1er septembre</li>
+      </ul>
+    </div>
+  </div>
+</body>
---
+++ Site internet/Base.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="fr">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
+<script src="script.js"></script>
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>Accueil Migrants Valfleury</title>
+<link href="static/media/favicon.png" type="image/png" rel="icon">
+<meta content="Des bénévoles se mobilisent pour accueillir humainement les migrants" name="Description">
+<meta content="benevoles, accueil, migrants, valfleury, loire, 42, citoyen, collectif, groupe, action, animation, culturel, humanitaire" name="Keywords">
+<meta content="Benevoles Accueil Migrants Valfleury" name="Author">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+<head>
+  <div style="text-align: center; font-family: Arial" class="form-group mx-auto">
+    <a href="index.html"><img title="Pour une Terre humaine" style="width: 1100px; height: 110px;" alt="Bandeau Pour une Terre humaine" src="static/media/bandeau.jpg"></a>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item form-inline">
+          <a class="nav-link" href="#">Actions <span class="sr-only"></span></a>
+          <a class="nav-link" href="#">Médias <span class="sr-only"></span></a>
+          <a class="nav-link" href="#">Nous rejoindre<span class="sr-only"></span></a>
+          <a class="nav-link" href="#">Nouveautés <img src="static/media/rss.png"><span class="sr-only"></span></a>
+          <a class="nav-link" href="#">Intranet <span class="sr-only"</span></a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Chercher sur le site" aria-label="Search">
+        <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Chercher</button>
+      </form>
+    </div>
+  </nav>
+</head>
+<footer>
+  <div class="col-sm-12 row">
+    <div class="text-left col-sm-4">
+      <a target="_blank" href="../legal.html">Mentions légales</a>
+    </div>
+    <div class="text-center col-sm-4">
+      Contact : accueilmigrantsvalfy@free.fr
+    </div>
+    <div class="text-right col-sm-4">
+      Dernière mise à jour : 25-8-2017
+    </div>
+  </div>
+</footer>
+</html>
