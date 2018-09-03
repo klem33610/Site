fbd12cde7b29364b2ce7b3f1ecc77b685713303d
klem <klem33@gmail.com>
2018-09-03 20:25:00 +0200

    Footer 	nouveau fichier : Site internet/Footer.php

---
+++ Site internet/Footer.php
@@ -0,0 +1,13 @@
+<footer>
+  <div class="col-sm-12 row">
+    <div class="text-left col-sm-4">
+      <a target="_blank" href="../legal.html">Mentions légales</a>
+    </div>
+    <div class="text-center col-sm-4">
+      Contact : accueilmigrantsvalfy@free.fr
+    </div>
+    <div class="text-right col-sm-4">
+      Dernière mise à jour : 25-8-2017
+    </div>
+  </div>
+</footer>
