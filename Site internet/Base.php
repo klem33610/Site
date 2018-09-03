c27279c696cab199d7211b55b7bb5a9adfde74a8
klem <klem33@gmail.com>
2018-09-03 20:19:35 +0200

    Création du footer 	modifié :         Site internet/Base.php 	Site internet/Footer.php

--- Site internet/Base.php
+++ Site internet/Base.php
@@ -40,17 +40,5 @@
     </div>
   </nav>
 </head>
-<footer>
-  <div class="col-sm-12 row">
-    <div class="text-left col-sm-4">
-      <a target="_blank" href="../legal.html">Mentions légales</a>
-    </div>
-    <div class="text-center col-sm-4">
-      Contact : accueilmigrantsvalfy@free.fr
-    </div>
-    <div class="text-right col-sm-4">
-      Dernière mise à jour : 25-8-2017
-    </div>
-  </div>
-</footer>
+<?php include('Footer.php'); ?>
 </html>
