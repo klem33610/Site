620dd6c73fbcba948c12c72f8879e541f2c82fa4
Clément DESAGE <clement.desage@rte-france.com>
2018-09-18 09:20:19 +0200

    Init page de gestion de données + accueil
    Optimisation code

---
+++ Site internet/Accueil.php
@@ -0,0 +1,1 @@
+<?php include('Base.php'); ?>
---
+++ Site internet/Intranet_Suivi.php
@@ -0,0 +1,4 @@
+<?php include('Base.php'); ?>
+<!-- Gestion et suivi : Membres et compétences/ans, Donneurs par type (AU ou A),
+calendrier des dons, Promesse et versement, Inscription par année,
+Relance mails : réinscription, rappel de dons, News, reçus fiscaux -->
