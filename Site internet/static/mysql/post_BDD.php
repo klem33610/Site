ccfd92c4af164b2585f46f94abf8617eaba764ea
klem <klem33@gmail.com>
2018-09-19 00:38:42 +0200

    Test 	modifié :         Site internet/static/mysql/post_BDD.php

--- Site internet/static/mysql/post_BDD.php
+++ Site internet/static/mysql/post_BDD.php
@@ -41,11 +41,10 @@ $Date = date('l jS \of F Y h:i:s A');
 
 $sql = 'SELECT COUNT(*) AS nb FROM la_table';
 $result = $bdd->query($sql);
-$number_of_rows = $result->fetchColumn();
-echo "<script>console.log({$number_of_rows})</script>";
+echo "<script>console.log({$result})</script>";
 
 // On affiche chaque entrée une à une
-if ($number_of_rows > 0)
+if ($result > 0)
 {
   $reponse = $bdd->query('SELECT * FROM Adhérents JS');
   $reponse->execute();
@@ -60,13 +59,12 @@ if ($number_of_rows > 0)
   	   ));
      }
    }
+   $reponse->closeCursor(); // Termine le traitement de la requête
   } else {
   // Insertion du message à l'aide d'une requête préparée
   $req = $bdd->prepare('INSERT INTO Adhérents (id, Nom, Prenom, Mail) VALUES(?, ?, ?, ?)');
   $req->execute(array($id, $Nom, $Prenom, $Mail));
   }
-$reponse->closeCursor(); // Termine le traitement de la requête
-
 ?>
 <!-- Tables  : Adhérents, Membres actifs, Aide_urgence et periode(1er versement - dernier versement), Dons, Dons_mensuels-->
 <!-- Gestion et suivi : Membres et compétences/ans, Donneurs par type (AU ou A),
