f15be52d624708c7ef9468947ef2ddcb2852864b
Clément DESAGE <clement.desage@rte-france.com>
2018-09-27 14:40:28 +0200

    Nouvelle timeline

--- Site internet/Timeline.php
+++ Site internet/Timeline.php
@@ -1,49 +1,41 @@
+<link href="static/css/timeline.min.css" rel="stylesheet">
+<script src="static/JS/timeline.js"></script>
+
 <?php date_default_timezone_set('UTC');
 setlocale(LC_TIME, 'fr_FR.utf8','fra');?>
+
   <div onclick="show_Table(this)">
-    <section class="section intro">
-      <div class="text-center card-header px-4 rounded-bottom py-2 bg-warning text-white shadow-sm">
+      <div class="mb-2 text-center card-header px-4 rounded-bottom py-2 bg-warning text-white shadow-sm">
         <h1>Frise temporelle des dons pour l'aide d'urgence &rarr;</h1>
       </div>
-    </section>
-
-    <section class="timeline" style="background-color:#456990">
-
-      <ol>
-        <? for ($i = 12; $i > 0; $i--) {
-          $Date = new DateTime(date("d-m-Y"));
-           $Month = $Date->modify('-'.$i.' months'); ?>
-        <li class="text-center <?php if($i > 3) { echo 'other_text';} ?>">
-          <div>
-              <p><strong><? echo strftime("%B %Y", strtotime($Month->format("F - Y"))); ?><br> </strong></p>
-              <p>Somme des promesses de dons :</p>
-              <p> 100 euros </p>
+      <div class="timeline">
+        <div class="timeline__wrap">
+          <div class="timeline__items">
+            <? for ($i = 12; $i > 0; $i--) {
+              $Date = new DateTime(date("d-m-Y"));
+               $Month = $Date->modify('-'.$i.' months'); ?>
+               <div class="timeline__item">
+                 <div class="text-center timeline__content">
+                   <h2><? echo strftime("%B %Y", strtotime($Month->format("F - Y"))); ?><br></h2>
+                   <p>Somme des promesses de dons :</p>
+                  <p> 100 euros </p>
+                </div>
+              </div>
+            <? } ?>
+            <? for ($i = 0; $i <= 12; $i++) {
+              $Date = new DateTime(date("d-m-Y"));
+               $Month = $Date->modify('+'.$i.' months'); ?>
+               <div class="timeline__item">
+                 <div class="<? if ($i==0) {echo "border border-warning rounded ";}?> text-center timeline__content">
+                   <h2><? echo strftime("%B %Y", strtotime($Month->format("F - Y"))); ?><br></h2>
+                   <p>Somme des promesses de dons :</p>
+                  <p> 100 euros </p>
+                </div>
+              </div>
+            <? } ?>
           </div>
-        </li>
-        <? } ?>
-        <? for ($i = 0; $i <= 12; $i++) {
-          $Date = new DateTime(date("d-m-Y"));
-           $Month = $Date->modify('+'.$i.' months'); ?>
-        <li class="text-center <?php if($i > 3) { echo 'other_text';} ?>">
-          <div class="<? if ($i == 0) {echo 'bg-warning';} ?>">
-              <p><strong><? echo strftime("%B %Y", strtotime($Month->format("F - Y"))); ?><br> </strong></p>
-              <p>Somme des promesses de dons :</p>
-              <p> 100 euros </p>
-          </div>
-        </li>
-        <? } ?>
-          <li></li>
-      </ol>
-
-      <div class="arrows">
-        <button class="arrow arrow__prev disabled" disabled>
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/arrow_prev.svg" alt="prev timeline arrow">
-        </button>
-        <button class="arrow arrow__next">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/arrow_next.svg" alt="next timeline arrow">
-        </button>
+        </div>
       </div>
-    </section>
-  </div>
+    </div>
 
-<script src="static/JS/Timeline.js"></script>
+<script src="static/JS/timeline.min.js"></script>
