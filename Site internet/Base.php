bd8dbfbe2862852e0671a75d2214d0f314584169
Clément DESAGE <clement.desage@rte-france.com>
2018-09-28 09:38:59 +0200

    navbar collapse et système d'authentification intranet

--- Site internet/Base.php
+++ Site internet/Base.php
@@ -24,6 +24,9 @@
     <a href="index.html"><img class="img-fluid" title="Pour une Terre humaine" style="width: 1100px; height: 110px;" alt="Bandeau Pour une Terre humaine" src="static/media/bandeau.jpg"></a>
   </div>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
